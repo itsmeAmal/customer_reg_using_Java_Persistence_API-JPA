f8810e980b9d33577be6dd907ac353c689946ac0
Amal <wickramarathna.amal@gmail.com>
2020-07-06 20:19:09 +0530

    transaction example with saving customer object into database

--- src/main/java/com/test/jpa/App.java
+++ src/main/java/com/test/jpa/App.java
@@ -7,12 +7,19 @@ import javax.persistence.Persistence;
 public class App {
 	
 	public static void main(String[] ar) {
+		
+		Customer customer = new Customer();
+		customer.setCustContact("111");
+		customer.setCustName("sihath");
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
 		EntityManager em = emf.createEntityManager();
 		
-		Customer customer = em.find(Customer.class, 2); 
-		System.out.print(customer); 
+		em.getTransaction().begin();
+		em.persist(customer); 
+		em.getTransaction().commit();
+//		Customer customer = em.find(Customer.class, 2); 
+//		System.out.print(customer); 
 		
 	}
 
